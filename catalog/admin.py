fe51ecac5a6d18277edfa2322bb479ca06e5987e
Rocco <grog512new@mail.ru>
2024-06-08 22:31:41 +0300

    Задание 4

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,12 +7,12 @@ from .models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ('id', 'name', 'description')
     search_fields = ('name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'category', 'price', 'created_at', 'updated_at')
     list_filter = ('category', 'created_at', 'updated_at')
     search_fields = ('name', 'description')
--- catalog/models.py
+++ catalog/models.py
@@ -17,7 +17,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
-    image = models.ImageField(upload_to='products/', verbose_name='Изображение (превью)')
+    image = models.ImageField(upload_to='products/', verbose_name='Изображение (превью)', null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
