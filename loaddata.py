2d3e19f514f177eefc51e8b4f4386c83488f986c
Rocco <grog512new@mail.ru>
2024-06-09 00:38:12 +0300

    Задание 6

--- loaddata.py
+++ loaddata.py
@@ -5,12 +5,10 @@ from django.core.management.base import BaseCommand
 from catalog.models import Product, Category
 
 
-def setup_django():
-    # Установите переменную окружения DJANGO_SETTINGS_MODULE
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-    # Загрузите настройки Django
-    django.setup()
-
+# def setup_django():
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+#     django.setup()
+#
 
 class Command(BaseCommand):
     help = 'Populate the database with initial data'
@@ -29,16 +27,12 @@ class Command(BaseCommand):
         return Command.json_read('catalog/fixtures/products.json')
 
     def handle(self, *args, **options):
-        # Удалить все продукты
         Product.objects.all().delete()
-        # Удалить все категории
         Category.objects.all().delete()
 
-        # Создать списки для хранения объектов
         category_for_create = []
         product_for_create = []
 
-        # Обойти все значения категорий из фикстуры для получения информации об одном объекте
         for category_data in self.json_read_categories():
             fields = category_data['fields']
             category_for_create.append(
@@ -49,10 +43,8 @@ class Command(BaseCommand):
                 )
             )
 
-        # Создать объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
 
-        # Обойти все значения продуктов из фикстуры для получения информации об одном объекте
         for product_data in self.json_read_products():
             fields = product_data['fields']
             product_for_create.append(
@@ -68,9 +60,12 @@ class Command(BaseCommand):
                 )
             )
 
-        # Создать объекты в базе с помощью метода bulk_create()
         Product.objects.bulk_create(product_for_create)
 
-        self.stdout.write(self.style.SUCCESS('Database has been populated successfully.'))
+        self.stdout.write(self.style.SUCCESS('!!! Successfully created initial data!!!'))
 
 
+
+# (venv) PS D:\Skypro\sky_Django> python manage.py loaddata .\catalog\fixtures\products.json
+# Installed 3 object(s) from 1 fixture(s)
+# (venv) PS D:\Skypro\sky_Django>
